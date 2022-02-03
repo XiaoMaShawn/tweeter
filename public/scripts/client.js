c88af0d9c91cba7b4b457e18e6b966b952e5574d
XiaoMaShawn <ma2014xiao@hotmail.com>
2022-02-03 14:33:52 -0500

    pre-submit version of APP

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,33 +4,6 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-
-
-// const tweetData = [
-//   {
-//     "user": {
-//       "name": "Newton",
-//       "avatars": "https://i.imgur.com/73hZDYK.png",
-//       "handle": "@SirIsaac"
-//     },
-//     "content": {
-//       "text": "If I have seen further it is by standing on the shoulders of giants"
-//     },
-//     "created_at": 1643639899091
-//   },
-//   {
-//     "user": {
-//       "name": "Descartes",
-//       "avatars": "https://i.imgur.com/nlhLi3I.png",
-//       "handle": "@rd"
-//     },
-//     "content": {
-//       "text": "Je pense , donc je suis"
-//     },
-//     "created_at": 1643726299091
-//   }
-// ]
-
 const renderTweets = function(tweetData) {
   tweetData.forEach((tweet) => {
     $('#post-board').append(createTweetElement(tweet));
